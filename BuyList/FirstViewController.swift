9585f2766e67b545ed3ccfe142a6bdd1520a9e00
i <i@MacBook-Pro-i.local>
2018-01-15 20:55:08 +0200

    2

--- BuyList/FirstViewController.swift
+++ BuyList/FirstViewController.swift
@@ -8,18 +8,67 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class FirstViewController: UIViewController, UITextFieldDelegate {
+  
+  @IBOutlet weak var itemTextField: UITextField!
+  
+  @IBOutlet weak var label: UILabel!
+  
+  @IBAction func add(_ sender: Any) {
+    
+    // проверка ввода пустого текста
+    
+    if itemTextField.text?.isEmpty ?? true {
+      return label.text = "Поле ввода не должно быть пустым"
+    }else {
+      label.text = ""
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    let itemsObject = UserDefaults.standard.object(forKey: "items")
+    
+    var items:[String]
+    
+    if  let tempItems = itemsObject as? [String] {
+      
+      items = tempItems
+      
+      items.append(itemTextField.text!)
+      
+    } else {
+      
+      items = [itemTextField.text!]
     }
-
-
+    
+    UserDefaults.standard.set(items, forKey: "items")
+    
+    itemTextField.text = ""
+    
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+    self.view.endEditing(true)
+    
+  }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    
+    textField.resignFirstResponder()
+    
+    return true
+    
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
 }
 
