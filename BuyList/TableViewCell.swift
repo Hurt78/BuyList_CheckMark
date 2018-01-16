b355f8efe77718086180f014be8df3efbb6e6471
i <i@MacBook-Pro-i.local>
2018-01-16 10:10:08 +0200

    BuyList_CheckMark

---
+++ BuyList/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  BuyList
+//
+//  Created by i on 16.01.2018.
+//  Copyright © 2018 i.niko. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
